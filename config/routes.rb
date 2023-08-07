5ca2e86224915d895d395b2818152c18226f2b92
ikoote1 <ikootepreim@gmail.com>
2023-08-07 16:40:52 +0300

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "home#index"
+  root to: "splash#index"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
