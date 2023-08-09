793613ad84f9fafe89f410b3d7b370196d5d0da4
ikoote1 <ikootepreim@gmail.com>
2023-08-09 03:08:23 +0300

    styling

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "splash#index"
 
+  get "categories" , to: "categories#index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
