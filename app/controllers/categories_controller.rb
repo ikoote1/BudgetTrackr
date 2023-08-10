6a7f462b5fa7c8f4a00f59765972843af08871b7
ikoote1 <ikootepreim@gmail.com>
2023-08-10 13:05:58 +0300

    final touches

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,12 +3,16 @@ class CategoriesController < ApplicationController
 
   # GET /categories or /categories.json
   def index
-    @categories = Category.where(user_id: current_user.id)
-    # @balances = Balance.where(author_id: current_user.id).includes([:items]).order('created_at DESC')
+    @categories = Category.where(user_id: current_user.id).includes([:items]).order('created_at DESC')
   end
 
   # GET /categories/1 or /categories/1.json
   def show
+    if current_user.id.to_i == Category.find(params[:id]).user_id.to_i
+      @category = Category.find(params[:id])
+    else
+      redirect_to categories_path
+    end
   end
 
   # GET /categories/new
@@ -25,35 +29,10 @@ class CategoriesController < ApplicationController
     @category = current_user.categories.build(category_params)
     respond_to do |format|
       if @category.save
-        format.html { redirect_to category_url(@category), notice: "Category was successfully created." }
-        format.json { render :show, status: :created, location: @category }
+        redirect_to custom_categories_path, notice: "Category was successfully created."
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
+        render :new, status: :unprocessable_entity
       end
-    end
-  end
-
-  # PATCH/PUT /categories/1 or /categories/1.json
-  def update
-    respond_to do |format|
-      if @category.update(category_params)
-        format.html { redirect_to category_url(@category), notice: "Category was successfully updated." }
-        format.json { render :show, status: :ok, location: @category }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /categories/1 or /categories/1.json
-  def destroy
-    @category.destroy
-
-    respond_to do |format|
-      format.html { redirect_to categories_url, notice: "Category was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
--- app/views/items/_item.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! item, :id, :name, :amount, :user_id, :created_at, :updated_at
-json.url item_url(item, format: :json)
