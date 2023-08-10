3c4d4c346c445a264563e3fa8f8d112cdb208d4b
ikoote1 <ikootepreim@gmail.com>
2023-08-10 03:33:24 +0300

    tansaction update

--- db/migrate/20230809234255_create_categorized_categories.rb
+++ db/migrate/20230809234255_create_categorized_categories.rb
@@ -1,7 +1,7 @@
 class CreateCategorizedCategories < ActiveRecord::Migration[7.0]
   def change
     create_table :categorized_categories do |t|
-      t.references :categorized_transaction, null: false, foreign_key: { to_table: :transactions }
+      t.references :transaction, null: false, foreign_key: true
       t.references :category, null: false, foreign_key: true
 
       t.timestamps
