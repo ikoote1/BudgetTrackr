d5b2306938763466c475d8290b2a4f5dfbc9119f
ikoote1 <ikootepreim@gmail.com>
2023-08-10 14:21:37 +0300

    final touches

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,63 +1,42 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: %i[ show edit update destroy ]
 
-  # GET /items or /items.json
-  def index
-    @items = Item.all
-  end
-
-  # GET /items/1 or /items/1.json
-  def show
-  end
-
   # GET /items/new
   def new
-    @item = Item.new
-  end
-
-  # GET /items/1/edit
-  def edit
+    if current_user.id.to_i == Category.find(params[:category_id]).user_id.to_i
+      @item = Item.new
+      @categories = Category.where(user_id: current_user.id)
+    else
+      redirect_to '/'
+    end
   end
 
   # POST /items or /items.json
   def create
-    @item = Item.new(item_params)
-
-    respond_to do |format|
-      if @item.save
-        format.html { redirect_to item_url(@item), notice: "Item was successfully created." }
-        format.json { render :show, status: :created, location: @item }
+    @item = Item.new(name: params[:item][:name], amount: params[:item][:amount], user_id: current_user.id)
+    if params[:item][:selected_ids]
+      if save_item_and_categorization
+        redirect_to categories_path(params[:category_id])
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
+        render :new, status: :unprocessable_entity
       end
-    end
-  end
-
-  # PATCH/PUT /items/1 or /items/1.json
-  def update
-    respond_to do |format|
-      if @item.update(item_params)
-        format.html { redirect_to item_url(@item), notice: "Item was successfully updated." }
-        format.json { render :show, status: :ok, location: @item }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /items/1 or /items/1.json
-  def destroy
-    @item.destroy
-
-    respond_to do |format|
-      format.html { redirect_to items_url, notice: "Item was successfully destroyed." }
-      format.json { head :no_content }
+    else
+      redirect_to Categorizations_path(params[:category_id]), alert: 'please select at least one checkbox.'
     end
   end
 
   private
+
+  def save_item_and_categorization
+    ActiveRecord::Base.transaction do
+      @item.save
+      params[:item][:selected_ids].each do |id|
+        Categorization.create(item_id: @item.id, category_id: id.to_i)
+      end
+    end
+  rescue ActiveRecord::RecordInvalid
+    false
+  end
     # Use callbacks to share common setup or constraints between actions.
     def set_item
       @item = Item.find(params[:id])
@@ -65,6 +44,6 @@ class ItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def item_params
-      params.require(:item).permit(:name, :amount, :user_id)
+      params.require(:item).permit(:name, :amount, selected_id: [])
     end
 end
